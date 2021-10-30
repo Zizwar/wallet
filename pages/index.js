e75959947d267847cb268ac71a78f755cca97490
Zizwar <zizwar@gmail.com>
2021-10-30 02:13:22 +0000

    update data wallet

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from 'next/image';
 export default function Home({ resJson = [] }) {
 const {data = []} = resJson;
   return (
@@ -13,12 +13,12 @@ const {data = []} = resJson;
               <div className="summary-balance">
                 {data.meta?.totalusd}
               </div>
-              <div className="summary-text-2">
-                +&nbsp;280.00
+              <div className="summary-text-2" dangerouslySetInnerHTML={{__html: data.meta?.usdpercentagechange}}>
+              
               </div>
             </div>
             <div className="user-profile">
-              <Image src="https://pbs.twimg.com/profile_images/834493671785525249/XdLjsJX_.jpg"  height="100%"  width="100%" className="user-photo" />
+              <Image src="/logo.png"  height="100%"  width="100%" className="user-photo" />
             </div>
           </div>
           <div className="content">
@@ -26,11 +26,11 @@ const {data = []} = resJson;
               <div className="upper-row">
                 <div className="card-item">
                   <span>Active Balance</span>
-                  <span>1&nbsp;804 <span className="dollar">$</span></span>
+                  <span> {data.meta?.totaleth} <span className="dollar">ETH</span></span>
                 </div>
                 <div className="card-item">
                   <span>My Save it</span>
-                  <span>2&nbsp;000 <span className="dollar">$</span></span>
+                  <span> {data.meta?.totalusd}<span className="dollar">$</span></span>
                 </div>
               </div>
               <div className="lower-row">
@@ -52,20 +52,44 @@ const {data = []} = resJson;
                 </div>
               </div>
             </div>
-            <div className="transactions"><span className="t-desc">Recent Transactions</span>
-              <div className="transaction">
-                <div className="t-icon-container"></div>
-                <div className="t-details">
-                  <div className="t-title">99 designs</div>
-                  <div className="t-time">03.45PM
-                  </div>
-                </div>
-                <div className="t-amount">+&nbsp;750$
-                </div>
-              </div>
+            <div className="transactions">
+              <span className="t-desc">Tokens</span>
+
+             
 
               <div className="transaction">
-                <div className="t-icon-container"><Image src="https://www.paypalobjects.com/webstatic/icon/pp144.png" className="t-icon" height="100%" width="100%"/></div>
+                <div className="t-icon-container">
+                  <Image src="https://www.paypalobjects.com/webstatic/icon/pp144.png" className="t-icon" height="100%" width="100%"/></div>
+                <div className="t-details">
+                  <div className="t-title">Paypal </div>
+                  <div className="t-time">03.45 AM
+                  </div>
+                </div>
+                <div className="t-amount">+&nbsp;200$
+                </div>
+              </div> <div className="transaction">
+                <div className="t-icon-container">
+                  <Image src="https://www.paypalobjects.com/webstatic/icon/pp144.png" className="t-icon" height="100%" width="100%"/></div>
+                <div className="t-details">
+                  <div className="t-title">Paypal </div>
+                  <div className="t-time">03.45 AM
+                  </div>
+                </div>
+                <div className="t-amount">+&nbsp;200$
+                </div>
+              </div> <div className="transaction">
+                <div className="t-icon-container">
+                  <Image src="https://www.paypalobjects.com/webstatic/icon/pp144.png" className="t-icon" height="100%" width="100%"/></div>
+                <div className="t-details">
+                  <div className="t-title">Paypal </div>
+                  <div className="t-time">03.45 AM
+                  </div>
+                </div>
+                <div className="t-amount">+&nbsp;200$
+                </div>
+              </div> <div className="transaction">
+                <div className="t-icon-container">
+                  <Image src="https://www.paypalobjects.com/webstatic/icon/pp144.png" className="t-icon" height="100%" width="100%"/></div>
                 <div className="t-details">
                   <div className="t-title">Paypal </div>
                   <div className="t-time">03.45 AM
@@ -76,7 +100,8 @@ const {data = []} = resJson;
               </div>
 
               <div className="transaction">
-                <div className="t-icon-container"><Image src="https://cdn.dribbble.com/assets/dribbble-ball-192-ec064e49e6f63d9a5fa911518781bee0c90688d052a038f8876ef0824f65eaf2.png" height="100%" width="100%" className="t-icon" /></div>
+                <div className="t-icon-container">
+                  <Image src="https://cdn.dribbble.com/assets/dribbble-ball-192-ec064e49e6f63d9a5fa911518781bee0c90688d052a038f8876ef0824f65eaf2.png" height="100%" width="100%" className="t-icon" /></div>
                 <div className="t-details">
                   <div className="t-title">99 designs</div>
                   <div className="t-time">08.00PM
