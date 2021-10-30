19770f921f43e19b9f386d0feeb954f7eb68faf2
Zizwar <zizwar@gmail.com>
2021-10-30 00:13:47 +0000

    update _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+
+      <title>Sloughi - Waallet</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel='stylesheet' href='https://pro.fontawesome.com/releases/v5.7.0/css/all.css' />
+
+    </Head>
+
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
